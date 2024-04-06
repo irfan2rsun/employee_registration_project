494fe06fc409f13bfff7ad1f650de6dca222f7d4
Irfan Tursun <149962565+irfan2rsun@users.noreply.github.com>
2024-04-06 12:54:13 -0700

    Added Back-End Validation & Restriction Annotations

--- src/main/java/com/cydeo/model/Employee.java
+++ src/main/java/com/cydeo/model/Employee.java
@@ -3,6 +3,10 @@ package com.cydeo.model;
 import lombok.*;
 import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
 @Getter
@@ -12,6 +16,16 @@ import java.time.LocalDate;
 @ToString
 public class Employee {
 
+//    @NotNull        --->    Field shouldn't be null
+//    @NotEmpty       --->    Field shouldn't be ""
+//    @NotBlank       --->    Field shouldn't be "       "
+
+//    @NotNull -> @NotNull
+//    @NotEmpty -> @NotNull + @NotEmpty
+//    @NotBlank -> @NotNull + @NotEmpty + @NotBlank
+
+    @NotBlank
+    @Size(max = 12, min = 2)
     private String firstName;
     private String lastName;
 
